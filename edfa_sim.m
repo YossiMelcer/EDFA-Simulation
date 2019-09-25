fe28fdc2460d4c2475f4ff65724b60b1d7db9172
Melcer-Neufeld, Joseph <zceejme@ucl.ac.uk>
2019-09-25 19:31:54 +0100

    changes2

--- edfa_sim.m
+++ edfa_sim.m
@@ -1,7 +1,7 @@
 % General Parameters
 
 h = 6.626e-34; % Planck's contstant
-c = 3e8; %Speed of light in a vacuum
+c = 3e8; %Speed of light in vacuum
 
 % Signal/Pump Parameters
 
@@ -13,3 +13,16 @@ Ps = 30e-6; % Power of signal input (W)
 % System Parameters
 
 L = 10; % Fibre length (m)
+dia = 5.5e-6; % Fiber diameter (m)
+tau = 10e-3; % Upper state lifetime (s)
+dlam = 2e-9; % Optical sampling bandwidth (m)
+
+wl = [wlp (1450e-9:dlam:1600e-9)]';% [m] wavelength vector
+[sig12, sig21] = GetErSpectrum(wl); % Absorption/Emission spectrums
+Gamma = 0.722; % 0.722 assuming 80% core overlap with Gaussian distributed intensity cross-section
+
+
+alph = 6.5; % Fiber Absorption (dB/m)
+Ntot = ConvAbsDB2N(s.alph,GetErSpectrum(1530)); % Doping concentration 
+
+aseTerm = GetASETerm(GammaS,sig21,lam,h,c,dlam,Ntot); 
