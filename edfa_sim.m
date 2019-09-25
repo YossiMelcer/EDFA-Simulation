c105634986edbba898c3c7c360d4fccbf2a68aa2
Melcer-Neufeld, Joseph <zceejme@ucl.ac.uk>
2019-09-25 14:47:51 +0100

    changes

--- edfa_sim.m
+++ edfa_sim.m
@@ -5,5 +5,11 @@ c = 3e8; %Speed of light in a vacuum
 
 % Signal/Pump Parameters
 
-wlp = 980e-9; % Pump Wavelength
-wls = 1535e-9; % Signal Wavelength
+wlp = 980e-9; % Pump Wavelength (m)
+wls = 1535e-9; % Signal Wavelength (m)
+Pp = 100e-3; % Power of pump input (W)
+Ps = 30e-6; % Power of signal input (W)
+
+% System Parameters
+
+L = 10; % Fibre length (m)
