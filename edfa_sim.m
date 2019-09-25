8bbef3bdd43c6485504bf3b6d4fd052b9655aad3
Melcer-Neufeld, Joseph <zceejme@ucl.ac.uk>
2019-09-25 14:33:52 +0100

    add edfa_sim

---
+++ edfa_sim.m
@@ -0,0 +1,9 @@
+% General Parameters
+
+h = 6.626e-34; % Planck's contstant
+c = 3e8; %Speed of light in a vacuum
+
+% Signal/Pump Parameters
+
+wlp = 980e-9; % Pump Wavelength
+wls = 1535e-9; % Signal Wavelength
